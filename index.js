69db144d09f26bd9205bf24398d5aa40095e8fd2
Mohammed Essam <memaq1998.10.7@gmail.com>
2023-02-14 17:06:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 5;
+let num2 = 12.4;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100);
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(5, 10, 15, 20, 6);
 
-// Write your code here
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
